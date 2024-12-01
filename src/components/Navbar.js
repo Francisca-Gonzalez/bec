b35be9230c78cdc7b8e1a7cb35a2d1be240baa32
Francisca-Gonzalez <75760416+Francisca-Gonzalez@users.noreply.github.com>
2024-11-30 23:43:47 -0300

    aaaa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,49 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "../styles/Navbar.css";
+// import logo from '../assets/logo.png';
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar navbar-expand-lg">
+//       <div className="container-fluid">
+//         <Link className="navbar-brand" to="/" style={{fontWeight: "bold"}}>
+//           <img src={logo} alt="logo" style={{width: "60px", paddingLeft: "0.6rem", paddingRight: "0.6rem"}}/>
+//           BEC
+//         </Link>
+//         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//           <span class="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//           <ul className="navbar-nav w-100 mb-2 mb-lg-0 align-items-center justify-content-center justify-content-lg-start">
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/">
+//                 Inicio
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/catalog">
+//                 Catálogo Completo
+//               </Link>
+//             </li>
+//           </ul>
+//           <div className="nav-login ms-lg-auto d-flex justify-content-center">
+//             <Link className="nav-link d-flex align-items-center" to="/login">
+//               <i className="bi bi-person-circle"></i> Ingresar
+//             </Link>
+//           </div>
+//         </div>
+
+
+
+
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
@@ -6,16 +52,41 @@ import logo from '../assets/logo.png';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/" style={{fontWeight: "bold"}}>
-          <img src={logo} alt="logo" style={{width: "50px", paddingRight: "0.6rem"}}/>
+      <div className="container">
+        {/* Logo y Título */}
+        <Link className="navbar-brand" to="/" style={{ fontWeight: "bold" }}>
+          <img
+            src={logo}
+            alt="logo"
+            style={{
+              width: "60px",
+              paddingLeft: "0.6rem",
+              paddingRight: "0.6rem",
+            }}
+          />
           BEC
         </Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+
+        {/* Botón Toggler */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav w-100 mb-2 mb-lg-0 align-items-center justify-content-center justify-content-lg-start">
+
+        {/* Menú de Navegación */}
+        <div
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
+        >
+          {/* Opciones Centrales */}
+          <ul className="navbar-nav mb-2 mb-lg-0 align-items-center justify-content-center w-100">
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Inicio
@@ -27,16 +98,17 @@ const Navbar = () => {
               </Link>
             </li>
           </ul>
+
+          {/* Login */}
           <div className="nav-login ms-lg-auto d-flex justify-content-center">
-            <Link className="nav-link d-flex align-items-center" to="/login">
+            <Link
+              className="nav-link d-flex align-items-center"
+              to="/login"
+            >
               <i className="bi bi-person-circle"></i> Ingresar
             </Link>
           </div>
         </div>
-
-
-
-
       </div>
     </nav>
   );
